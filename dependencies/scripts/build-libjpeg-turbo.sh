196ceddc6755eb6a4ca33c238527e436c627ad4d
Tom Rothamel <tom@rothamel.us>
2014-12-04 08:37:00 -0500

    Upgrade libjpeg-turbo and get building on all platforms.
    At the moment, libjpeg-turbo doesn't seem to contain SIMD code for
    arm64, so we build without SIMD on that platform and try to let the
    native CPU speed carry the day.

--- dependencies/scripts/build-libjpeg-turbo.sh
+++ dependencies/scripts/build-libjpeg-turbo.sh
@@ -4,32 +4,55 @@
 
 export PATH="$PATH:`pwd`"
 
-if [ ! -f $CACHEROOT/libjpeg-turbo-1.2.0.tar.gz ]; then
+if [ ! -f $CACHEROOT/libjpeg-turbo-$LIBJPEG_TURBO_VERSION.tar.gz ]; then
   echo 'Downloading libjpeg-turbo source'
-  try curl -L http://downloads.sourceforge.net/project/libjpeg-turbo/1.2.0/libjpeg-turbo-1.2.0.tar.gz > $CACHEROOT/libjpeg-turbo-1.2.0.tar.gz
+  try curl -L http://downloads.sourceforge.net/project/libjpeg-turbo/$LIBJPEG_TURBO_VERSION/libjpeg-turbo-$LIBJPEG_TURBO_VERSION.tar.gz > $CACHEROOT/libjpeg-turbo-$LIBJPEG_TURBO_VERSION.tar.gz
 fi
 
-try rm -rf $TMPROOT/libjpeg-turbo-1.2.0
+try rm -rf $TMPROOT/libjpeg-turbo-$LIBJPEG_TURBO_VERSION
 echo 'Extracting libjpeg-turbo source'
-try tar xjf $CACHEROOT/libjpeg-turbo-1.2.0.tar.gz 2>&1 >/dev/null
-try mv libjpeg-turbo-1.2.0 $TMPROOT
+try tar xjf $CACHEROOT/libjpeg-turbo-$LIBJPEG_TURBO_VERSION.tar.gz 2>&1 >/dev/null
+try mv libjpeg-turbo-$LIBJPEG_TURBO_VERSION $TMPROOT
 
-pushd $TMPROOT/libjpeg-turbo-1.2.0
+pushd $TMPROOT/libjpeg-turbo-$LIBJPEG_TURBO_VERSION
 
 echo 'Configuring libjpeg-turbo'
 try autoreconf -fiv 2>&1 >/dev/null
 
 # Under Xcode 5, this compile fails if -O is set to anything other than 0.
-try ./configure --prefix=$DESTROOT \
-  --with-jpeg8 \
-  --host="$ARM_HOST" \
-  --enable-static \
-  --disable-shared \
-  CC="$ARM_REAL_CC" AR="$ARM_AR" \
-  LDFLAGS="-no-integrated-as $ARM_LDFLAGS" \
-  CFLAGS="-no-integrated-as $ARM_CFLAGS -O0" \
-  CPPFLAGS="$CFLAGS"
-  CCASFLAGS="-no-integrated-as $ARM_CFLAGS" 2>&1 >/dev/null
+
+# The simd assembly hasn't been ported to arm64. We live without it and hope
+# the CPU is fast.
+if [ "$RENIOSARCH" = "arm64" ] ; then
+
+	try ./configure --prefix=$DESTROOT \
+	  --with-jpeg8 \
+	  --host="$ARM_HOST" \
+	  --enable-static \
+	  --disable-shared \
+	  --without-simd \
+	  CC="$ARM_REAL_CC" AR="$ARM_AR" \
+	  CFLAGS="$ARM_CFLAGS" \
+	  LDFLAGS="$ARM_LDFLAGS" \
+	  CCASFLAGS="$ARM_CFLAGS" \
+	  CPPFLAGS="$ARM_CFLAGS"
+else
+
+    try ./configure --prefix=$DESTROOT \
+      --with-jpeg8 \
+      --host="$ARM_HOST" \
+      --enable-static \
+      --disable-shared \
+      CC="$ARM_REAL_CC" AR="$ARM_AR" \
+      LDFLAGS="-no-integrated-as $ARM_LDFLAGS" \
+      CFLAGS="-no-integrated-as $ARM_CFLAGS -O0" \
+      CCASFLAGS="-no-integrated-as $ARM_CFLAGS" \
+      CPPFLAGS="$ARM_CFLAGS"
+
+            #        CFLAGS="-no-integrated-as $ARM_CFLAGS -O0" \
+
+fi
+    
 
 try make clean 2>&1 >/dev/null
 echo 'Building libjpeg-turbo'
