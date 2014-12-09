5a5336f7361a49498eed128bf002fb3b70339de6
Tom Rothamel <tom@rothamel.us>
2014-12-08 19:54:38 -0500

    Build ctypes with python.
    pyobjus depends on it, and it's also nice to have.

--- dependencies/scripts/build-all.sh
+++ dependencies/scripts/build-all.sh
@@ -2,6 +2,7 @@
 
 . $(dirname $0)/utils.sh
 
+try $(dirname $0)/build-libffi.sh
 try $(dirname $0)/build-python.sh
 try $(dirname $0)/build-sdl.sh
 try $(dirname $0)/build-libpng.sh
@@ -11,6 +12,5 @@ try $(dirname $0)/build-freetype.sh
 try $(dirname $0)/build-sdl2_ttf.sh
 try $(dirname $0)/build-sdl2_image.sh
 try $(dirname $0)/build-sdl2_gfx.sh
-try $(dirname $0)/build-libffi.sh
 try $(dirname $0)/build-pyobjus.sh
 try $(dirname $0)/build-pygame.sh
--- dependencies/scripts/build-python.sh
+++ dependencies/scripts/build-python.sh
@@ -56,14 +56,16 @@ mkdir -p extralibs
 xcrun -sdk $SDKBASENAME lipo "$IOSSDKROOT/usr/lib/libgcc_s.1.dylib" -thin $RENIOSARCH -output extralibs/libgcc_s.10.4.dylib 2>&1 >/dev/null
 
 try cp $RENIOSDEPROOT/src/python/ModulesSetup Modules/Setup.local
+try cat $RENIOSDEPROOT/src/python/ModulesSetup.mobile >> Modules/Setup.local
 
 try ./configure CC="$ARM_CC" LD="$ARM_LD" \
-  CFLAGS="$ARM_CFLAGS" \
-  LDFLAGS="$ARM_LDFLAGS -Lextralibs/" \
+  CFLAGS="$ARM_CFLAGS -I$BUILDROOT/include/ffi" \
+  LDFLAGS="$ARM_LDFLAGS -L$BUILDROOT/lib -Lextralibs/" \
   --disable-toolbox-glue \
   --host="$ARM_HOST" \
   --prefix=/python \
   --without-doc-strings 2>&1 >/dev/null
+
 
 try make HOSTPYTHON=./hostpython HOSTPGEN=./Parser/hostpgen \
      CROSS_COMPILE_TARGET=yes 2>&1 >/dev/null
