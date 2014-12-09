ad28c491d781a8c3bd7482aab7495c2be940ed5e
Tom Rothamel <tom@rothamel.us>
2014-12-08 23:50:12 -0500

    Fix SDL2_ttf build.
    We had been including all of freetype, including a supurious
    main().

--- dependencies/scripts/build-sdl2_ttf.sh
+++ dependencies/scripts/build-sdl2_ttf.sh
@@ -2,6 +2,8 @@
 
 . $(dirname $0)/utils.sh
 
+set -x
+
 if [ ! -f $CACHEROOT/SDL2_ttf-$SDL2_TTF_VERSION.tar.gz ]; then
     echo 'Downloading SDL2_ttf source'
     curl -L https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-$SDL2_TTF_VERSION.tar.gz > $CACHEROOT/SDL2_ttf-$SDL2_TTF_VERSION.tar.gz
@@ -14,19 +16,13 @@ echo 'Extracting SDL2_ttf source'
 try tar xzf $CACHEROOT/SDL2_ttf-$SDL2_TTF_VERSION.tar.gz -C $TMPROOT # 2>&1 >/dev/null
 try pushd $TMPROOT/SDL2_ttf-$SDL2_TTF_VERSION
 
-# Link SDL2 as SDL, so we can find it.
-rm $TMPROOT/SDL
-try ln -s $TMPROOT/SDL2-$SDL_VERSION $TMPROOT/SDL
 
-echo 'Building SDL2_ttf'
+for i in SDL_ttf.c; do
+    try $ARM_REAL_CC $ARM_CFLAGS -I$BUILDROOT/include/SDL2 -I$BUILDROOT/include/freetype -c $i
+done
 
-pushd $TMPROOT/SDL2_ttf-$SDL2_TTF_VERSION/Xcode-iOS
-try xcodebuild -project SDL_ttf.xcodeproj -configuration $RENIOSBUILDCONFIGURATION -sdk $SDKBASENAME$SDKVER -arch $RENIOSARCH clean
-try xcodebuild -project SDL_ttf.xcodeproj -configuration $RENIOSBUILDCONFIGURATION -sdk $SDKBASENAME$SDKVER -arch $RENIOSARCH 
-popd
+rm $BUILDROOT/lib/libSDL2_ttf.a
+try $ARM_AR rcs $BUILDROOT/lib/libSDL2_ttf.a *.o
+try cp -a SDL_*.h $BUILDROOT/include/SDL2
 
-echo "Moving SDL_ttf products into place"
-try cp Xcode-iOS/build/$RENIOSBUILDCONFIGURATION-$SDKBASENAME/libSDL2_ttf.a $BUILDROOT/lib
-try cp -a SDL_ttf.h $BUILDROOT/include
-
-popd
+try popd
