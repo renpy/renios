3ac8937f3b36fdc7f9d47e5ced3fd3cfedf82439
Tom Rothamel <tom@rothamel.us>
2015-01-17 05:03:07 -0500

    iap: Set and validate product identifiers.

--- prototype/IAPHelper.m
+++ prototype/IAPHelper.m
@@ -2,19 +2,54 @@
 
 @import StoreKit;
 
-@interface IAPHelper : NSObject {
-    
-}
+@interface IAPHelper : NSObject <SKProductsRequestDelegate>
 
+@property NSArray *productIdentifiers;
+@property NSMutableDictionary *products;
+
+- (id) init;
 - (BOOL) canMakePayments;
+- (void) validateProductIdentifiers;
 
 @end;
 
 
 @implementation IAPHelper
 
+- (id) init {
+    self = [ super init ];
+    self.products = [[ NSMutableDictionary alloc] init];
+    return self;
+}
+
 - (BOOL) canMakePayments {
     return [SKPaymentQueue canMakePayments];
+}
+
+- (void)validateProductIdentifiers {
+ 
+    for (NSString *i in self.productIdentifiers) {
+        printf("%s\n", [i UTF8String]);
+    }
+    
+    SKProductsRequest *productsRequest = [[SKProductsRequest alloc]
+                                          initWithProductIdentifiers:[NSSet setWithArray: self.productIdentifiers]];
+    productsRequest.delegate = self;
+    [productsRequest start];
+}
+
+// SKProductsRequestDelegate protocol method
+- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response {
+
+    for (SKProduct *prod in response.products) {
+        // printf("OK %s %f\n", [ prod.productIdentifier UTF8String ], prod.price.doubleValue);
+
+        [ self.products setObject: prod forKey: prod.productIdentifier ];
+    }
+    
+//    for (NSString *s in response.invalidProductIdentifiers) {
+//        printf("INVALID %s\n", [ s UTF8String ]);
+//    }
 }
 
 @end
