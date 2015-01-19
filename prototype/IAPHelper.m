72d3e87caad46c60ca25bd2a4bad040fb09ceddd
Tom Rothamel <tom@rothamel.us>
2015-01-18 20:49:46 -0500

    iap: Purchasing and restoring purchases.

--- prototype/IAPHelper.m
+++ prototype/IAPHelper.m
@@ -4,14 +4,27 @@
 
 @interface IAPHelper : NSObject <SKProductsRequestDelegate, SKPaymentTransactionObserver>
 
+// An array of product identifier NSStrings. This is set when we're first constructed.
 @property NSArray *productIdentifiers;
+
+// A map from a product identifier NSString to the corresponding project.
 @property NSMutableDictionary *products;
-@property int status;
+
+// 1 if an operation is in progress. 0 if no operation is in progress.
+@property int finished;
+
+// The set of identifiers for purchased products.
+@property NSMutableSet *purchased;
+
+// The set of identifiers for deferred products.
+@property NSMutableSet *deferred;
 
 - (id) init;
 - (BOOL) canMakePayments;
 - (void) validateProductIdentifiers;
 - (void) beginPurchase: (NSString *) identifier;
+- (BOOL) hasPurchased: (NSString *) identifier;
+- (BOOL) isDeferred: (NSString *) identifier;
 @end;
 
 
@@ -19,8 +32,10 @@
 
 - (id) init {
     self = [ super init ];
-    self.products = [[ NSMutableDictionary alloc] init];
-    self.status = 2;
+    self.products = [ [ NSMutableDictionary alloc] init];
+    self.purchased = [ [ NSMutableSet alloc ] init ];
+    self.deferred = [ [ NSMutableSet alloc ] init ];
+    self.finished = 1;
 
     [[SKPaymentQueue defaultQueue] addTransactionObserver: self];
     
@@ -28,36 +43,34 @@
 }
 
 - (BOOL) canMakePayments {
-    // printf("Can make payments: %d\n", [SKPaymentQueue canMakePayments]);
-    
     return [SKPaymentQueue canMakePayments];
 }
 
 - (void) validateProductIdentifiers {
  
-    for (NSString *i in self.productIdentifiers) {
-        printf("%s\n", [i UTF8String]);
-    }
-    
     SKProductsRequest *productsRequest = [[SKProductsRequest alloc]
                                           initWithProductIdentifiers:[NSSet setWithArray: self.productIdentifiers]];
     productsRequest.delegate = self;
     [productsRequest start];
 }
 
+- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response {
+    for (SKProduct *prod in response.products) {
+        [ self.products setObject: prod forKey: prod.productIdentifier ];
+    }
+}
+
+
 - (void) beginPurchase: (NSString *) identifier {
-    self.status = 0;
-    
-    printf("Begin purchase of %s\n", [ identifier UTF8String ]);
     
     SKProduct *product = [ self.products objectForKey: identifier ];
+    
     if (product == nil) {
-        printf("Product not found.\n");
-        self.status = 2;
         return;
     }
+
+    self.finished = 0;
     
-    printf("Product found.\n");
     SKMutablePayment *payment = [ SKMutablePayment paymentWithProduct: product ];
     payment.quantity = 1;
     
@@ -65,50 +78,70 @@
     
 }
 
-// SKProductsRequestDelegate protocol method
-- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response {
-
-    for (SKProduct *prod in response.products) {
-        printf("OK %s %f\n", [ prod.productIdentifier UTF8String ], prod.price.doubleValue);
-
-        [ self.products setObject: prod forKey: prod.productIdentifier ];
-    }
+- (void) restorePurchases {
+    self.finished = 0;
     
-//    for (NSString *s in response.invalidProductIdentifiers) {
-//        printf("INVALID %s\n", [ s UTF8String ]);
-//    }
+    [[SKPaymentQueue defaultQueue] restoreCompletedTransactions ];
+    printf("Restore started.\n");
 }
-
 
 - (void) paymentQueue: (SKPaymentQueue *) queue updatedTransactions: (NSArray *) transactions {
     for (SKPaymentTransaction *t in transactions) {
+        NSString *identifier = t.payment.productIdentifier;
+        
         switch (t.transactionState) {
             case SKPaymentTransactionStatePurchased:
-                printf("Purchased %s\n", [t.payment.productIdentifier UTF8String ]);
-                self.status = 1;
+                printf("Purchased %s\n", [ identifier UTF8String ]);
+                [ self.deferred removeObject: identifier ];
+                [ self.purchased addObject: identifier ];
+                [ [ SKPaymentQueue defaultQueue] finishTransaction: t ];
+                self.finished = 1;
                 break;
 
             case SKPaymentTransactionStateFailed:
-                printf("Failed %s\n", [t.payment.productIdentifier UTF8String ]);
-                self.status = 1;
+                printf("Failed %s\n", [ identifier UTF8String ]);
+                [ [ SKPaymentQueue defaultQueue] finishTransaction: t ];
+                self.finished = 1;
                 break;
 
             case SKPaymentTransactionStateRestored:
-                printf("Restored %s\n", [t.payment.productIdentifier UTF8String ]);
-                self.status = 1;
+                printf("Restored %s\n", [ identifier UTF8String ]);
+                [ self.deferred removeObject: identifier ];
+                [ self.purchased addObject: identifier ];
+                [ [ SKPaymentQueue defaultQueue] finishTransaction: t ];
                 break;
             
             case SKPaymentTransactionStatePurchasing:
-                printf("Purchasing %s\n", [t.payment.productIdentifier UTF8String ]);
-                self.status = 1;
+                printf("Purchasing %s\n", [ identifier UTF8String ]);
                 break;
                 
             case SKPaymentTransactionStateDeferred:
-                printf("Deferred %s\n", [t.payment.productIdentifier UTF8String ]);
-                self.status = 1;
+                printf("Deferred %s\n", [ identifier UTF8String ]);
+                [ self.deferred addObject: identifier ];
+                self.finished = 1;
                 break;
         }
     }
 }
+
+- (void) paymentQueue: (SKPaymentQueue *)queue restoreCompletedTransactionsFailedWithError: (NSError *) error {
+    printf("Restore failed with error.\n");
+    self.finished = 1;
+}
+
+- (void) paymentQueueRestoreCompletedTransactionsFinished: (SKPaymentQueue *) queue {
+    printf("Restore completed.\n");
+    self.finished = 1;
+}
+
+
+- (BOOL) hasPurchased: (NSString *) identifier {
+    return [ self.purchased member: identifier ] != nil;
+}
+
+- (BOOL) isDeferred: (NSString *) identifier {
+    return [ self.deferred member: identifier ] != nil;
+}
+
 
 @end
