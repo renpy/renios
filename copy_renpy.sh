f8d14256bacd62515df604c045932f37ccab5e5b
Tom Rothamel <tom@rothamel.us>
2015-03-04 22:02:11 -0500

    Parameterize copy_renpy with a game path.

--- copy_renpy.sh
+++ copy_renpy.sh
@@ -6,12 +6,12 @@ renpy=/home/tom/ab/renpy
 prototype=$(dirname $(readlink -f $0))/prototype
 
 pushd $renpy
-$renpy/run.sh $renpy/the_question quit
+$renpy/run.sh "$1" quit
 popd
 
 rm -Rf $prototype/base || true
 mkdir $prototype/base
 
-cp -a --no-preserve=ownership $renpy/iaptest/game $prototype/base
+cp -a --no-preserve=ownership "$1"/game $prototype/base
 cp -a --no-preserve=ownership $renpy/renpy $prototype/base
 cp -a --no-preserve=ownership $renpy/renpy.py $prototype/base/main.py
