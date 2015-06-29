e376edcd918844ff90bd592a445177e719347898
Tom Rothamel <tom@rothamel.us>
2015-06-28 22:44:38 -0400

    Use NSLog to log stdout and stderr.

--- dependencies/iossupport/iossupport.py
+++ dependencies/iossupport/iossupport.py
@@ -1,10 +1,31 @@
+from pyobjus import autoclass, objc_str
+
+Log = autoclass('Log')
+
+class LogFile(object):
+
+    def __init__(self):
+        self.buffer = ''
+
+    def write(self, s):
+        s = self.buffer + s
+        lines = s.split("\n")
+        for l in lines[:-1]:
+            Log.log_(l)
+        self.buffer = lines[-1]
+
+    def flush(self):
+        return
+
+import sys
+sys.stdout = sys.stderr = LogFile()
+
 def open_url(url):
 
     if url.startswith("file:"):
         raise Exception("Opening file urls is not supported: " + url)
 
     try:
-        from pyobjus import autoclass, objc_str
         NSURL = autoclass('NSURL')
         UIApplication = autoclass("UIApplication")
 
